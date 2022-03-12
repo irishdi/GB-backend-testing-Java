1a8f1fda4773e05e1e91fb5f0a1aec8c25804ff8
irishdi <irishdi@inbox.ru>
2022-03-12 13:30:16 +0300

    Lesson_6

---
+++ Ира Учеба/BackendTesting_Java/src/main/java/Lesson_6/service/CategoryService.java
@@ -0,0 +1,13 @@
+package Lesson_6.service;
+
+import Lesson_5.dto.CategoryResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface CategoryService {
+
+    @GET("categories/{id}")
+    Call<CategoryResponse> getCategory(@Path("id") int id);
+
+}
---
+++ Ира Учеба/BackendTesting_Java/src/main/java/Lesson_6/utils/RetrofitUtils.java
@@ -0,0 +1,57 @@
+package Lesson_6.utils;
+
+import Lesson_5.utils.LoggingInterceptor;
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static okhttp3.logging.HttpLoggingInterceptor.Level.BODY;
+
+@UtilityClass
+public class RetrofitUtils {
+
+    Properties properties = new Properties();
+    private static InputStream configFile;
+
+    static {
+        try {
+            configFile = new FileInputStream("src/main/resources/my.properties");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SneakyThrows
+    public String getBaseUrl() {
+        properties.load(configFile);
+        return properties.getProperty("url");
+    }
+    //создание экземпляра объекта перехватчика
+    HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+    //другой перехватчик
+    Lesson_5.utils.LoggingInterceptor logging2 = new LoggingInterceptor();
+    //создание клиента
+    OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+    public Retrofit getRetrofit(){
+        //уровень логирования
+        logging.setLevel(BODY);
+        //перехватчик в okhttp-клиент
+        httpClient.addInterceptor(logging);
+        return new Retrofit.Builder()
+                .baseUrl(getBaseUrl())
+                .addConverterFactory(JacksonConverterFactory.create())
+                //добавление okhttp-клиента к retrofit-клиенту
+                .client(httpClient.build())
+                .build();
+    }
+
+}
